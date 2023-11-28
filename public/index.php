c138086f645e8aeabe45c43cf64eebb7b7d254e2
=Fugushiva <2111jedelodder@student.epfc.eu>
2023-11-28 21:55:43 +0100

    add some new feature

--- public/index.php
+++ public/index.php
@@ -7,3 +7,18 @@ require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>Guestbook</title>
+</head>
+
+<body>
+    <img src="https://media.tenor.com/Ug6cbVA1ZsMAAAAd/developer.gif">
+</body>
+
+</html>
